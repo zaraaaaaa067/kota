46ede8b1e20e6e83a37bb7b1280a660b215fb4d0
zaraaaaaa067 <zuguuuuu@gmail.com>
2026-07-22 23:35:50 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'KOTA',
+  webDir: 'www'
+};
+
+export default config;
